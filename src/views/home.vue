f74fbe4f4e0fedfed127aa94ce5b457c6b21abb2
kromate <akpananthony33@gmail.com>
2022-04-19 20:29:52 +0100

    --- Auto-Git Commit ---

--- src/views/home.vue
+++ src/views/home.vue
@@ -63,8 +63,8 @@
 				<div class="flex justify-around items-center text-sub text-main_dark">
 					<router-link
 						v-for="{ path, icon, name, iconOutline } in [
-							{ name: 'Home', path: '/#', icon: home,  },
-							{ name: 'tasks', path: '/#', icon: helpCircle,  },
+							{ name: 'Home', path: '/#', icon: 'home',  },
+							{ name: 'tasks', path: '/#', icon: 'wallet',  },
 							{ name: 'chat', path: '/#', icon: receipt, },
 							{ name: 'setting', path: '/#', icon: library, },
 
