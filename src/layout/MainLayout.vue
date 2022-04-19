fcca5d578ba2169d235e31ea0883d07f3553eb65
kromate <akpananthony33@gmail.com>
2022-04-19 20:36:45 +0100

    --- Auto-Git Commit ---

--- src/layout/MainLayout.vue
+++ src/layout/MainLayout.vue
@@ -8,7 +8,7 @@
 			<IonToolbar class="lg:hidden pt-0.5">
 				<div class="flex justify-around items-center text-sub text-main_dark">
 					<router-link
-						v-for="{ path, icon, name, iconOutline } in [
+						v-for="{ path, icon, name } in [
 							{ name: 'Home', path: '/#', icon: 'home',  },
 							{ name: 'tasks', path: '/#', icon: 'wallet',  },
 							{ name: 'chat', path: '/#', icon: 'comment-dots', },
@@ -17,7 +17,7 @@
 						]" :key="path"
 						:to="path"
 						class="col-span-1 flex items-center justify-center">
-						<i :class=`las` + `la-${cog}`></i>
+						<i :class="[`las` `la-${icon}`]"></i>
 						<IonText>{{ name }}</IonText>
 					</router-link>
 				</div>
@@ -28,9 +28,7 @@
 </template>
 
 <script lang="ts" setup>
-import { IonFooter, IonIcon, IonToolbar } from '@ionic/vue'
+import { IonFooter, IonToolbar } from '@ionic/vue'
 </script>
 
-<style  scoped>
-
-</style>
+					
