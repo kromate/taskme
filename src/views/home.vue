8f1909858f05d999ce7672094bd0f9cc8c242fd2
kromate <akpananthony33@gmail.com>
2022-04-19 13:17:23 +0100

    --- Auto-Git Commit ---

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,7 +1,11 @@
 <template>
-	<ion-page class="text-purple-500 text-2xl bg-white" >
-		<div>
+	<ion-page class="text-purple-500 text-2xl bg-white px-7 py-8" >
+		<div class="flex">
 			<Avatar/>
+			<span>
+				<ion-text> Good morning</ion-text> <br>
+				<ion-text class="text-base">Rebecca</ion-text>
+			</span>
 		</div>
 
 		<i class="lar la-bell"></i>
