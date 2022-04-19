339f6dc37387590e6796190215309f1ba3561b54
kromate <akpananthony33@gmail.com>
2022-04-19 13:06:47 +0100

    --- Auto-Git Commit ---

--- src/plugin/registerGlobally.ts
+++ src/plugin/registerGlobally.ts
@@ -1,6 +1,6 @@
 import { IonButton, IonContent, IonIcon, IonInput, IonLabel, IonPage, IonText } from '@ionic/vue';
 
-export const registerIonicComponent = (async ({ app }) => {
+export const registerIonicComponent = async (app) => {
     app.component('IonPage', IonPage)
     app.component('IonContent', IonContent)
     app.component('IonButton', IonButton)
@@ -8,16 +8,19 @@ export const registerIonicComponent = (async ({ app }) => {
     app.component('IonIcon', IonIcon)
     app.component('IonText', IonText)
     app.component('IonLabel', IonLabel)
-})
+}
 
 export const registerComponents = async (app: any) => {
+    await registerIonicComponent(app)
     const requireComponent = import.meta.glob('../components/*.vue')
+
+    console.log(requireComponent);
 
     for (const path in requireComponent) {
         requireComponent[path]().then(async (mod) => {
 
             const componentName = path.split('/')?.pop()?.replace(/\.\w+$/, '')
-
+            console.log(componentName);
 
             await app.component(
                 componentName,
