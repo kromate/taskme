6da7d63d0c868aa028555cb94871cb89bf9aa407
kromate <akpananthony33@gmail.com>
2022-04-19 04:25:27 +0100

    --- Auto-Git Commit ---

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
 		'@typescript-eslint/interface-name-prefix': 'off',
 		'no-empty-function': 'off',
 		'no-undef': 'off',
+		'vue/multi-word-component-names': 'off',
 		'@typescript-eslint/no-empty-function': 'off',
 		'@typescript-eslint/no-explicit-any': 'off',
 		'@typescript-eslint/camelcase': 'off',
