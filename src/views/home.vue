ff7a04ca7d515593d4c7e74c753ee4e1c006afac
kromate <akpananthony33@gmail.com>
2022-04-19 17:54:23 +0100

    --- Auto-Git Commit ---

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,21 +1,20 @@
 <template>
 	<MainLayout>
-		<div class="py-8 mx-7">
-			<div class="w-full flex justify-between items-center">
-				<div class="flex items-center">
-					<Avatar class="w-11 h-11 mr-3"/>
-					<span class="flex flex-col">
-						<ion-text class="text-grey text-xs font-medium"> Good morning</ion-text> 
-						<ion-text class="text-base text-primary font-medium">Rebecca</ion-text>
-					</span>
-				</div>
 
-				<i class="lar la-bell text-primary border p-3 rounded-full"></i>
+		<div class="w-full flex justify-between items-center pt-8 px-7">
+			<div class="flex items-center">
+				<Avatar class="w-11 h-11 mr-3"/>
+				<span class="flex flex-col">
+					<ion-text class="text-grey text-xs font-medium"> Good morning</ion-text> 
+					<ion-text class="text-base text-primary font-medium">Rebecca</ion-text>
+				</span>
 			</div>
-
-			<div class="bg-purple w-full h-48 mt-12 -mx-7">
-			</div>
+			<i class="lar la-bell text-primary border p-3 rounded-full"></i>
 		</div>
+
+		<div class="bg-purple w-full h-48 mt-12 ">
+		</div>
+
 	
 	</MainLayout>
 	
