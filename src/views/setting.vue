a4fe7b8089df73d0afd73a7c75b378465a4b4f5f
kromate <akpananthony33@gmail.com>
2022-04-21 10:05:07 +0100

    --- Auto-Git Commit ---

--- src/views/setting.vue
+++ src/views/setting.vue
@@ -3,7 +3,7 @@
 		<div class="bg-lightPurplebg min-h-screen px-4 py-8">
 			<div class="flex justify-center items-center">
 				<ion-text class=" text-xl font-extrabold text-center">Profile</ion-text>
-				<i class="lar la-bell text-primary bg-white p-3 rounded-full ml-auto absolute right-6"></i>
+				<i class="lar la-bell text-primary bg-white p-3 rounded-full ml-auto absolute right-6  "></i>
 			</div>
 			<Avatar class="w-28 h-28 mx-auto mt-8"/>
 		</div>
