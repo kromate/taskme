21afa88693b1f964b53866b1e8d342e35a724946
kromate <akpananthony33@gmail.com>
2022-04-21 19:51:06 +0100

    --- Auto-Git Commit ---

--- src/views/setting.vue
+++ src/views/setting.vue
@@ -1,6 +1,6 @@
 <template>
 	<MainLayout>
-		<div class="bg-lightPurplebg min-h-screen px-5 py-8 flex flex-col">
+		<div class="bg-lightPurplebg min-h-screen px-5 py-8 flex flex-col pb-20">
 			<div class="flex justify-center items-center">
 				<ion-text class=" text-xl font-extrabold text-center">Profile</ion-text>
 				<i class="lar la-bell text-primary bg-white p-3 rounded-full ml-auto absolute right-6  "></i>
@@ -14,10 +14,11 @@
 				<button class="font-medium bg-white text-purple text-xs py-2 px-4 rounded-full mt-2 w-fit">Copy referral code</button>
 			</div>	
 
-			<div class="bg-white mt-6 p-7 rounded-3xl">
-				<div class="flex items-center">
+			<div class="bg-white mt-6 p-4 rounded-3xl">
+				<div class="flex items-center border-b p-3 py-4 border-lightPurplebg" v-for="n in 5" :key="n">
 					<i class="lar la-user text-purple text-xl w-9 h-9 bg-lightPurple p-3 rounded-full justify-center flex items-center"></i>
 					<ion-text class="font-medium text-base ml-4">Profile info</ion-text>
+					<i class="las la-angle-right ml-auto"></i>
 
 				</div>
 			</div>
